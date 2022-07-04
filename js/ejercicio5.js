7f1104547218e5eadd4f6014a604bc43a5ff39ee
Eloy Pintos <eloy_pintos14@hotmail.com>
2022-07-04 09:55:03 -0300

    modificando ejercicio 4

--- js/ejercicio5.js
+++ js/ejercicio5.js
@@ -1,5 +1,5 @@
 var n1 , n2 , resultado;
-n1 = prompt("Introduce primer numero");
-n2 = prompt("Introduce segundo numero");
+let n1 = prompt("Introduce primer numero");
+let n2 = prompt("Introduce segundo numero");
 resultado = (parseInt(n1) + parseInt(n2));
-alert ("El resultado es : "+ resultado);
+document.write = (resultado)
